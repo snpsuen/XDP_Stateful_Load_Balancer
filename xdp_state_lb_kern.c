cf45ccb288741d578acbff5719499662cff2b3ce
Sunny Paul Suen <66404197+snpsuen@users.noreply.github.com>
2023-06-14 02:19:56 +0800

    Update xdp_state_lb_kern.c

--- xdp_state_lb_kern.c
+++ xdp_state_lb_kern.c
@@ -2,10 +2,9 @@
 
 #define IP_ADDRESS(x) (unsigned int)(172 + (17 << 8) + (0 << 16) + (x << 24))
 
-#define BACKEND_A 2
-#define BACKEND_B 3
-#define CLIENT 4
-#define LB 5
+#define LB 2
+#define BACKEND_A 3
+#define BACKEND_B 4
 
 struct five_tuple {
     __u8  protocol;
